86136ac0eeb65be6208b55fcbc127de034daef7e
*ttjygBJmmO <kanazawa.takuya@salpis.net>
2020-04-08 23:23:05 +0900

    cms13

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-
+/*
 //import React from "react"
 import React from 'react';
 import PropTypes from "prop-types"
@@ -41,6 +41,58 @@ const classes = useStyles();
       <div>
         <main className={classes.main}>{children}</main>
         <footer>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
+*/
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
@@ -80,4 +81,45 @@ export const query = graphql`
     }
   }
 `
+export default IndexPage
+*/
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <h1>Hello, Gatsby!</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <article key={node.frontmatter.slug}>
+        <h2>
+          <Link to={`/${node.frontmatter.slug}`}>
+            {node.frontmatter.title}
+          </Link>
+        </h2>
+        <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+        <p>{node.excerpt}</p>
+      </article>
+    ))}
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "YYYY年MM月DD日")
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
 export default IndexPage
