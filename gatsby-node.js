30f90ad8d2884377f86a8490a2f8b47b97696628
*ttjygBJmmO <kanazawa.takuya@salpis.net>
2020-04-08 23:41:44 +0900

    cms13

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,4 @@
 const path = require(`path`)
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
 
 exports.createPages = async gatsbyNodeHelpers => {
   const { graphql, actions } = gatsbyNodeHelpers
